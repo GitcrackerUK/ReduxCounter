cdf4dc2c1fbabcc8579bb50594cb1e87c1040a54
pawel.siwek <pawel.siwek@filament.uk.com>
2021-04-16 17:22:47 +0100

    imported provider and store

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import './App.css';
 import Counter from './components/Counter';
+import store from './components/store';
+import { Provider } from 'react-redux';
+import './App.css';
+
 function App() {
     return (
-        <div className='App'>
-            <header className='App-header'>
-                <h1>Redux</h1>
-            </header>
-        </div>
+        <Provider store={store}>
+            <div className='App'>
+                <header className='App-header'>
+                    <h1>Redux</h1>
+                </header>
+                <Counter></Counter>
+            </div>
+        </Provider>
     );
 }
 
