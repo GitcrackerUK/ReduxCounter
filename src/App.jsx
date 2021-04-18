fa41d1aa713800ee34f6cf2c6692f3603f6fb43b
pawel.siwek <pawel.siwek@filament.uk.com>
2021-04-18 13:58:47 +0100

    added coments for Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import Counter from './components/Counter';
-import { store } from './components/store';
-import { Provider } from 'react-redux';
+import Counter from './components/Counter';  // imports component 
+import { store } from './components/store';  // import store 
+import { Provider } from 'react-redux'; // imports provider to connect store to App 
 import './App.css';
 
 function App() {
     return (
-        <Provider store={store}>
+        // Provider needs be implemented in order to connect App to the store. Provider need be wrapped around App.
+        <Provider store={store}>  
             <div className='App'>
                 <header className='App-header'>
                     <h1>Redux</h1>
